9a54139f1f30ca8f1d9fa2473a44e0c36c7a3376
JoseantoAlro <jalvrom2609@g.educaand.es>
2025-11-17 13:53:50 +0100

    nodemon con ejs

--- 03Express/01-express.js
+++ 03Express/01-express.js
@@ -2,15 +2,25 @@ const express = require('express')
 const app = express()
 const port = 3000
 
+app.set('view engine', 'ejs');
+app.set('pruebas', __dirname+'/prueba');
 app.use(express.static(__dirname+'/public'));    //acceso a carpeta public pero con la ruta absoluta
 
-app.get('/', (req, res) => {
-  console.log(__dirname)  //pinjtar ruta en consola
-  res.send('Hello World!')
+app.get('/pruebas', (req, res) => {
+  res.render("prueba", {titulo : "mi titulo dinamico"});
 })
+
+//app.get('/', (req, res) => {
+  //console.log(__dirname)  //pintar ruta en consola
+  //res.render("index", {titulo : "mi titulo dinamico"}) //renderizar el ejs
+//})
 app.get('/contacto', (req, res) => {
   res.send('contacto.html')
 })
+app.use((req,res) => {      //para cuando no encuentra una ruta
+  res.status(404).sendFile(__dirname+"/public//html/404.html")
+})
+
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
 })
