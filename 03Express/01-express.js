bf64e7a35c769138f0ff1876d2da6048604c0fbc
JoseantoAlro <jalvrom2609@g.educaand.es>
2025-12-04 13:40:25 +0100

    cosas de pokemon de express

--- 03Express/01-express.js
+++ 03Express/01-express.js
@@ -6,17 +6,14 @@ app.set('view engine', 'ejs');
 app.set('pruebas', __dirname+'/prueba');
 app.use(express.static(__dirname+'/public'));    //acceso a carpeta public pero con la ruta absoluta
 
-app.get('/pruebas', (req, res) => {  //send envia reuqest info y render desde nuestro servidor o algo asi
-  res.render("prueba", {titulo : "mi titulo dinamico"});
-})
+app.use('/', require('./router/rutas'));
+app.use('/contacto', require('./router/rutas'));
+app.use('/pokemon', require('./router/rutas'));
 
 //app.get('/', (req, res) => {
   //console.log(__dirname)  //pintar ruta en consola
   //res.render("index", {titulo : "mi titulo dinamico"}) //renderizar el ejs
 //})
-app.get('/contacto', (req, res) => {
-  res.send('contacto.html')
-})
 app.use((req,res) => {      //para cuando no encuentra una ruta
   res.status(404).sendFile(__dirname+"/public//html/404.html")
 })
---
+++ 03Express/router/rutas.js
@@ -0,0 +1,21 @@
+const express = require('express') //Requerimos Express
+const router = express.Router();
+
+// Ahora, CORTAMOS del fichero principal 01-express.js
+// las dos rutas que tenemos: la principal ( / ) y la 
+// de contactos ( /contaco )
+// Importante que ya no usaremos el app.get(...), ahora
+//vamos a utilizar las rutas, por lo que deberemos poner:
+router.get('/', (req, res) => {
+    res.render("index.ejs", { titulo: "mi titulo dinámico" })
+})
+
+router.get('/contacto', (req, res) => {
+    res.render("prueba.ejs", { titulo: "Estamos en contacto de manera dinámica!!" })
+})
+
+router.get('/pokemon', (req, res) => {
+    res.render("pokemon.ejs", { titulo: "Estamos en pokemon!" })
+})
+// Por último, vamos a exportarlo:
+module.exports = router;
